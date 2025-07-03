e42009e9bc167f0e2f1d418c50dc46e989a00b30
jihoon0213 <jihun8138@gmail.com>
2025-07-03 17:49:17 +0900

    2025-07-03 react

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App25 from "./App25.jsx";
+import App37 from "./App37.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App25 />
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")).render(<App37 />);
